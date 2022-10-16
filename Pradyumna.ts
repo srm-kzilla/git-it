0d87dfa6a6d3dd1f73981b0b7e0593366606413a
YARE0909 <94288311+YARE0909@users.noreply.github.com>
2022-10-16 16:27:41 +0530

    Created  new file
    Created a new file and added some basic ts code

---
+++ Pradyumna.ts
@@ -0,0 +1,5 @@
+var about: string = "Hello there! I am Pradyumna D from CSE (Data Science) First year. I am looking forward to working with you all. I usually do front end and here are 2 of my works!"
+var website_1: string = "https://pradyumna-d.vercel.app/";
+var website_2: string = "https://dank-assistant-dashboard.vercel.app/";
+
+console.log(`${about}\n${website_1}\n${website_2}`);
